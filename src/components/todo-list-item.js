1690365fe045e8c48824982bf060a50cc51729cc
sxweb <phpred@yandex.ru>
2021-07-14 10:35:45 +0300

    react components properties

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,7 +1,10 @@
 import React from "react";
 
-const TodoListItem = () => {
-    return <li>learn react elements</li>;
+const TodoListItem = ({label, important = false}) => {
+    const liStyle = {
+        color: important ? 'tomato' : 'black'
+    }
+    return <li style={liStyle}>{label}</li>;
 }
 
 export default TodoListItem;
--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -4,8 +4,10 @@ import TodoListItem from "./todo-list-item";
 const TodoList = () => {
     return(
         <ul>
-            <li><TodoListItem/></li>
-            <li><TodoListItem/></li>
+            <li><TodoListItem label='Drink coffee'/></li>
+            <li><TodoListItem
+                label = 'Create react app'
+                important/></li>
         </ul>
     );
 };
