b72d0a829aeec822831990fa3c029557df97f97b
sxweb <phpred@yandex.ru>
2021-07-16 12:40:16 +0300

    simple setstate

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -5,27 +5,43 @@ import './todo-list-item-label.css';
 export default class TodoListItem extends Component{
     constructor(props) {
         super(props);
+        this.state = {
+            done: false,
+            important: false
+        }
         this.onLabelClick =() => {
-            console.log(`Done ${this.props.label}`);
+            this.setState({
+                done: true
+            })
+        }
+        this.onMarkImportant =() =>{
+            this.setState({
+                important: true
+            })
         }
     }
 
     render(){
-        const {label, important = false} = this.props;
-        const liStyle = {
-            color: important ? 'tomato' : 'black'
+        const {label} = this.props;
+        const {done, important} = this.state;
+        let classNames = 'todo-list-item';
+        if(done){
+            classNames += ' done';
+        }
+        if(important){
+            classNames += ' important';
         }
         return (
-            <span className='todo-list-item'>
+            <span className={classNames}>
                         <span
                             className='todo-list-item-label'
-                            style={liStyle}
                             onClick={this.onLabelClick}>
                                 {label}
                         </span>
                         <button
                             type='button'
-                            className='btn btn-outline-success btn-sm'>
+                            className='btn btn-outline-success btn-sm'
+                            onClick={this.onMarkImportant}>
                             <i className='fa fa-exclamation'/>
                         </button>
                         <button
