3a5c5c3c8d5e9a82fe9f9bf274634787f4ca71af
sxweb <phpred@yandex.ru>
2021-07-16 11:35:05 +0300

    classes in React

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,31 +1,32 @@
-import React from "react";
+import React, {Component} from "react";
 import './todo-list-item.css';
 import './todo-list-item-label.css';
 
-const TodoListItem = ({label, important = false}) => {
-    const liStyle = {
-        color: important ? 'tomato' : 'black'
+export default class TodoListItem extends Component{
+    render(){
+        const {label, important = false} = this.props;
+        const liStyle = {
+            color: important ? 'tomato' : 'black'
+        }
+        return (
+            <span className='todo-list-item'>
+                        <span
+                            className='todo-list-item-label'
+                            style={liStyle}>
+                                {label}
+                        </span>
+                        <button
+                            type='button'
+                            className='btn btn-outline-success btn-sm'>
+                            <i className='fa fa-exclamation'/>
+                        </button>
+                        <button
+                            type='button'
+                            className='btn btn-outline-success btn-sm'>
+                            <i className="far fa-trash-alt"/>
+                        </button>
+                    </span>
+
+        );
     }
-    return (
-        <span className='todo-list-item'>
-            <span
-                className='todo-list-item-label'
-                style={liStyle}>
-                    {label}
-            </span>
-            <button
-                type='button'
-                className='btn btn-outline-success btn-sm'>
-                <i className='fa fa-exclamation'/>
-            </button>
-            <button
-                type='button'
-                className='btn btn-outline-success btn-sm'>
-                <i className="far fa-trash-alt"/>
-            </button>
-        </span>
-
-    );
 }
-
-export default TodoListItem;
